4e0a96cfcd1858ab2cd3d6a9d269ee2e8a6382c1
sujeet kumar <ksujeetkumar7678@gmail.con>
2024-03-02 19:06:11 +0530

    Refactor calcArrayWidth function to calculate the width for array inside array-container.tsx file

--- src/components/array-visual/array-container.tsx
+++ src/components/array-visual/array-container.tsx
@@ -1,29 +1,26 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import GridBackground from "../grid-bg";
 import { useSortVisualizer } from "@/context/visualizerUtils";
 import LineBar from "./line-bar";
 
 const ArrayContainer: React.FC = () => {
-  const { array, isSorting } = useSortVisualizer();
-  const containerRef = useRef<HTMLDivElement>(null);
+  const { array } = useSortVisualizer();
   const [barWidth, setBarWidth] = useState(0);
 
   const maxElement = Math.max(...array);
 
   const calcHeight = (ele: number) => {
-    return ((ele * 100) / maxElement).toFixed(2);
+    if (maxElement === 0) return 100;
+    return parseFloat(((ele * 100) / maxElement).toFixed(2));
   };
   console.log("render");
   /**
    * Calculates the width of the array container and updates the barWidth state.
    */
   const calcArrayWidth = useCallback(() => {
-    if (containerRef.current && !isSorting) {
-      const { width } = containerRef.current.getBoundingClientRect();
-      setBarWidth((width - 100) / array.length); // Update barWidth state
-    }
-  }, [array.length, isSorting]);
+    setBarWidth(100 / array.length); // Update barWidth state
+  }, [array.length]);
 
   useEffect(() => {
     window.addEventListener("resize", calcArrayWidth);
@@ -33,33 +30,28 @@ const ArrayContainer: React.FC = () => {
 
   useEffect(() => {
     calcArrayWidth(); // Recalculate barWidth when array length changes
-  }, [array.length, calcArrayWidth, containerRef.current?.clientWidth]);
+  }, [array.length, calcArrayWidth]);
 
   return (
     <GridBackground>
-      <div
-        className='w-full h-full relative'
-        ref={containerRef}
-      >
+      <div className='w-full h-full relative'>
         {/* represent array into line bar */}
-        <div>adslfajlj</div>
-        <div className='h-full pb-6'>
-          <div className='flex items-center justify-center gap-3 h-full'>
-            <AnimatePresence>
-              {array.map((ele, i) => {
-                const height = parseFloat(calcHeight(ele));
+        <div>Hello </div>
+        <div className='flex items-center justify-center gap-3 h-full pb-6'>
+          <AnimatePresence>
+            {array.map((ele, i) => {
+              const height = calcHeight(ele);
 
-                return (
-                  <LineBar
-                    key={i}
-                    arrElement={ele}
-                    barWidth={barWidth}
-                    barHeight={height}
-                  />
-                );
-              })}
-            </AnimatePresence>
-          </div>
+              return (
+                <LineBar
+                  key={i}
+                  arrElement={ele}
+                  barWidth={barWidth}
+                  barHeight={height}
+                />
+              );
+            })}
+          </AnimatePresence>
         </div>
       </div>
     </GridBackground>
--- src/components/array-visual/line-bar.tsx
+++ src/components/array-visual/line-bar.tsx
@@ -12,7 +12,7 @@ const LineBar: React.FC<Props> = ({ barWidth, barHeight }) => {
     <motion.div
       className='default-ele-bg array-line-bar transition-all duration-300 self-end ease-in-out'
       initial={{ width: 0, height: 0 }}
-      animate={{ width: barWidth, height: `${barHeight}%` }}
+      animate={{ width: `${barWidth}%`, height: `${barHeight}%` }}
       transition={{ duration: 0.2 }}
       exit={{ width: 0 }}
     ></motion.div>
